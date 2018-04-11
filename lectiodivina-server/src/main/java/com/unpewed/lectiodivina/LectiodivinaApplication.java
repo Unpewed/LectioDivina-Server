2dd02efdc67fb98ebd1fa273275a27e4cd45889d
Bjorn74 <benfogt@gmail.com>
2018-04-11 10:29:57 -0400

    Initial commit

---
+++ lectiodivina-server/src/main/java/com/unpewed/lectiodivina/LectiodivinaApplication.java
@@ -0,0 +1,12 @@
+package com.unpewed.lectiodivina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectiodivinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectiodivinaApplication.class, args);
+	}
+}
